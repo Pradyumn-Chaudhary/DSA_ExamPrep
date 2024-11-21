5969286176890330780b584a19370d2d2c00ec99
Pradyumn Chaudhary <pradyumnchaudhary6@gmail.com>
2024-11-21 13:17:54 +0530

    Selection Sort

--- selectionSort.c
+++ selectionSort.c
@@ -2,14 +2,19 @@
 
 int main(){
     int arr[] = {9, 5, 2, 4, 6, 1};
-    for (int i = 0; i < 5;i++){
+    for (int i = 0; i < 6 - 1;i++){
+        int k = i;
         for (int j = i+1;  j < 6;j++){
-            if(arr[i]>arr[j]){
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+            if(arr[k]>arr[j]){
+                k = j;
             }
         }
+        if(k == i){
+            break;
+        }
+        int temp = arr[k];
+        arr[k] = arr[i];
+        arr[i] = temp;
     }
     for (int i = 0; i < 6;i++){
         printf("%d\t", arr[i]);
