533e7dad45eead3a6d198447953edc1cae5a574b
Pradyumn Chaudhary <pradyumnchaudhary6@gmail.com>
2024-11-22 10:47:27 +0530

    Quick Sort

---
+++ quickSort.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+int partition(int arr[],int low,int high){
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j < high;j++){
+        if(arr[j]<pivot){
+            i++;
+            //swap
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+    //swap
+    i++;
+    int temp = arr[high];
+    arr[high] = arr[i];
+    arr[i] = temp;
+
+    return i; //pivot index
+}
+
+void quickSort(int arr[],int low,int high){
+    if(low<high){
+        int pidx = partition(arr, low, high);
+        quickSort(arr, low, pidx - 1);
+        quickSort(arr, pidx + 1, high);
+    }
+}
+
+int main(){
+    int arr[] = {6, 3, 9, 5, 2, 8};
+    int n = sizeof(arr) / sizeof(arr[0]); 
+    quickSort(arr, 0, n - 1);
+
+    for (int i = 0; i < n;i++){
+        printf("%d ", arr[i]);
+    }
+        return 0;
+}
